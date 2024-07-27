74f7b0f938c21c817da7db7687a671bd7595b4aa
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-07-28 00:36:31 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://iyanferdiansyach30:192234230@cluster0.b3lbk5r.mongodb.net/food-del"
+      ""
     )
     .then(() => console.log("DB Connected"));
 };
