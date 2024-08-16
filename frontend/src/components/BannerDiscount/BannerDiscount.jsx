4d79ad7f29a4eb4267db0d1d5c8c38f6cf582d2b
RafindraAditias <rafindra.aditias@gmail.com>
2024-08-16 15:31:25 +0700

    add banner promo

---
+++ frontend/src/components/BannerDiscount/BannerDiscount.jsx
@@ -0,0 +1,10 @@
+import "./BannerDiscount.css"
+const BannerDiscount = () => {
+  return (
+    <div className="banner-container">
+        <a href="/#" className="promo-button">See More</a>
+    </div>
+  )
+}
+
+export default BannerDiscount
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
+import BannerDiscount from '../../components/BannerDiscount/BannerDiscount';
 // import AppDownload from '../../components/AppDownload/AppDownload';
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
   return (
     <div>
       <Header/>
+      <BannerDiscount/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
       {/* <AppDownload/> */}
